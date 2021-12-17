ea05ab09d40bbae24f353fe054999e2c2a5d7dd5
Elena Voloshina <elen3011@yandex.ru>
2021-12-17 18:56:22 +0300

    исправлены замечания - адаптивные элементы главной страницы

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+const navSite = document.querySelector('.site-list');
+const navUser = document.querySelector('.user-list');
+const navToggle = document.querySelector('.navigation__toggle');
+
+navToggle.addEventListener('click', function() {
+  if (navSite.classList.contains('navigation__list--opened') && navUser.classList.contains('navigation__list--opened')) {
+    navToggle.classList.remove('navigation__toggle--closed');
+    navUser.classList.remove('navigation__list--opened');
+    navSite.classList.remove('navigation__list--opened');
+    navUser.classList.add('navigation__list--closed');
+    navSite.classList.add('navigation__list--closed');
+  } else {
+    navToggle.classList.add('navigation__toggle--closed');
+    navUser.classList.remove('navigation__list--closed');
+    navSite.classList.remove('navigation__list--closed');
+    navUser.classList.add('navigation__list--opened');
+    navSite.classList.add('navigation__list--opened');
+  }
+});
